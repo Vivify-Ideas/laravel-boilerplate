ee9f78f095cd1901d158ff0dc8b589be1ba83837
l3xq <cse.l3x@gmail.com>
2018-09-24 12:14:07 +0200

    Upgrade to Laravel 5.7

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -60,6 +60,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'oauthClient' => \App\Http\Middleware\OauthClient::class
+        'oauthClient' => \App\Http\Middleware\OauthClient::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class
     ];
 }
