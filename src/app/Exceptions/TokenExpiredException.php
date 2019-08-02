92bf3f7d3dc6510f3793110997146d6aea9c6e95
MrGagi <dragan1176@gmail.com>
2019-08-02 18:37:52 +0200

    Fix all our exceptions

--- src/app/Exceptions/TokenExpiredException.php
+++ src/app/Exceptions/TokenExpiredException.php
@@ -6,8 +6,8 @@ class TokenExpiredException extends BaseException {
     const STATUS_CODE = 401;
     const MESSAGE = 'Token has expired';
 
-    public function __construct()
+    public function __construct(...$context)
     {
-        parent::__construct(self::MESSAGE, self::STATUS_CODE);
+        parent::__construct(self::MESSAGE, self::STATUS_CODE, $context);
     }
 }
